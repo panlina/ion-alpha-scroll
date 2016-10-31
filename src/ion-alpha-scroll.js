f421302cc61a8cfc58f0e4e3e746bf33182fd459
Jack Lu <jacklu@jacklu.me>
2016-10-31 14:14:54 +0800

    refactor into ion-alpha-scroll, alpha-bar

--- src/ion-alpha-scroll.js
+++ src/ion-alpha-scroll.js
@@ -10,37 +10,18 @@ angular.module('ion-alpha-scroll', [])
 				var children = tElement.contents();
 				var template = angular.element([
 					'<ion-list class="ion_alpha_list_outer">',
-						'<ion-scroll delegate-handle="alphaScroll">',
-							'<div data-ng-repeat="(letter, items) in sorted_items" class="ion_alpha_list">',
-								'<ion-item class="item item-divider" id="index_{{letter}}">{{letter}}</ion-item>',
-								'<ion-item ng-repeat="item in items"></ion-item>',
-							'</div>',
-						'</ion-scroll>',
-						'<ul class="ion_alpha_sidebar">',
-							'<li ng-click="alphaScrollGoToList(\'index_{{letter}}\')" ng-repeat="letter in alphabet">{{$index%2 == 0 ? letter: "&bull;"}}</li>',
-						'</ul>',
+						'<div data-ng-repeat="(letter, items) in sorted_items" class="ion_alpha_list">',
+							'<ion-item class="item item-divider" id="index_{{letter}}">{{letter}}</ion-item>',
+							'<ion-item ng-repeat="item in items"></ion-item>',
+						'</div>',
 					'</ion-list>'
 					].join(''));
-
-				var headerHeight = $document[0].body.querySelector('.bar-header').offsetHeight;
-				var subHeaderHeight = tAttrs.subheader === "true" ? 44 : 0;
-				var tabHeight = $document[0].body.querySelector('.tab-nav') ? $document[0].body.querySelector('.tab-nav').offsetHeight : 0;
-				var windowHeight = window.innerHeight;
-
-				var contentHeight = windowHeight - headerHeight - subHeaderHeight - tabHeight;
 
 				angular.element(template.find('ion-item')[1]).append(children);
 				tElement.html('');
       			tElement.append(template);
 				
-      			tElement.find('ion-scroll').css({"height": contentHeight + 'px'});
-
 				return function (scope, element, attrs, ngModel) {
-					var count = 0;
-					var scrollContainer = element.find('ion-scroll');
-
-					var ionicScroll = scrollContainer.controller('$ionicScroll');
-
 	                // do nothing if the model is not set
 	                if (!ngModel) return;
 
@@ -91,4 +72,43 @@ angular.module('ion-alpha-scroll', [])
 	            }
 	        }
         };
-    }]);
+	}])
+.directive('alphaBar', [
+	'$ionicScrollDelegate', '$location', '$timeout', '$document',
+	function ($ionicScrollDelegate, $location, $timeout, $document) {
+		return {
+			require: '?ngModel',
+			restrict: 'AE',
+			replace: true,
+			compile: function (tElement, tAttrs, tTransclude) {
+				var children = tElement.contents();
+				var template = angular.element([
+					'<ul class="ion_alpha_sidebar">',
+						'<li ng-click="alphaScrollGoToList(\'index_{{letter}}\')" ng-repeat="letter in alphabet">{{$index%2 == 0 ? letter: "&bull;"}}</li>',
+					'</ul>'
+				].join(''));
+
+				var headerHeight = $document[0].body.querySelector('.bar-header').offsetHeight;
+				var subHeaderHeight = tAttrs.subheader === "true" ? 44 : 0;
+				var tabHeight = $document[0].body.querySelector('.tab-nav') ? $document[0].body.querySelector('.tab-nav').offsetHeight : 0;
+				var windowHeight = window.innerHeight;
+
+				var contentHeight = windowHeight - headerHeight - subHeaderHeight - tabHeight;
+
+				tElement.html('');
+				tElement.append(template);
+
+				tElement.find('ul').css({ "top": headerHeight + subHeaderHeight + tabHeight + 'px' });
+
+				return function (scope, element, attrs, ngModel) {
+					scope.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+
+					scope.alphaScrollGoToList = function (id) {
+						$location.hash(id);
+						$ionicScrollDelegate.$getByHandle(attrs.delegateHandle).anchorScroll();
+					}
+
+				}
+			}
+		};
+	}]);
